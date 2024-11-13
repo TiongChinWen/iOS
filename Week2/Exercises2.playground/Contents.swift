14b1233e9048dafdda1342be666b5d062b4c91e1
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-13 10:43:28 +0800

    Exe2

--- Week2/Exercises2.playground/Contents.swift
+++ Week2/Exercises2.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var numBottle:Int = 15
+print("The number of bottle is \(numBottle)")
+
+var weight:Double = 45.50
+print("Jackson's weight is \(weight)")
+
+let todayDate:Bool = true
+print("Today date is 13Nov, it is \(todayDate)")
+
+var book = "Harry Potter"
+print("My favourite book is \(book)")
