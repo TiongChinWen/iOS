704a33628789a04f83a93ae3a9568ba7701358ae
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-20 09:35:03 +0800

    Exe1.4

--- Week3/Exe1.4.playground/Contents.swift
+++ Week3/Exe1.4.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let age = 80
+
+if age >= 65 {
+    print("Ticket price is $8")
+}else if age>=13 {
+    print("Ticket price is $15")
+}else if age >= 5 {
+    print("Ticket price is $10")
+}else{
+    print("Free entry")
+}
