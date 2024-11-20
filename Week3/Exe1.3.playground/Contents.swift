8fb18feb409544e19010d3e16e4e507db1546726
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-20 09:29:10 +0800

    Exe2.1.3

--- Week3/Exe1.3.playground/Contents.swift
+++ Week3/Exe1.3.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let year = 2016
+
+if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+    print("\(year) is a Leap Year")
+
+}else{
+    print("\(year) is Not a Leap Year")
+}
