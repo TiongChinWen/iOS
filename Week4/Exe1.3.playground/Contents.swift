2ec6c2a9325d2d25678923ee20a382e3144d20fe
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-27 09:59:25 +0800

    Exe1.3

--- Week4/Exe1.3.playground/Contents.swift
+++ Week4/Exe1.3.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let numbers = [12,33,56,78,99,23,49,10]
+
+if let first = numbers.first{
+    var smallest = first
+    var larger = first
+    
+    for number in numbers{
+        if number < smallest{
+            smallest = number
+        }
+        if number > larger{
+            larger = number
+        }
+    }
+    
+    print("The smallest number is \(smallest)")
+    print("The larger number is \(larger)")
+}
