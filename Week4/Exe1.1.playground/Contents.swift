640493b01415fc9b1900240a98c1620de50fe2d3
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-27 09:43:54 +0800

    Exe1.2

--- Week4/Exe1.1.playground/Contents.swift
+++ Week4/Exe1.1.playground/Contents.swift
@@ -2,3 +2,4 @@ import UIKit
 for i in stride(from:1,through:50,by:2){
     print(i)
 }
+
--- Week4/Exe1.2.playground/Contents.swift
+++ Week4/Exe1.2.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var num = 2534
+let numString = String(num)
+var sum = 0
+for char in numString{
+    if let number = Int(String(char)){
+        sum += number
+    }
+}
+print("The sum is \(sum)")
