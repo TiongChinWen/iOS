c59be54feb6f2bbbe40642bed9fee1af426cbec9
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-27 11:19:19 +0800

    Exe1.6

--- Week4/Exe1.6.playground/Contents.swift
+++ Week4/Exe1.6.playground/Contents.swift
@@ -1,6 +1,16 @@
 import UIKit
+var num = 4
+for i in 1...num{
+    for _ in 1...i{
+        print("*", terminator: "")
+    }
+    print("")
+}
 
-let star = 5
-for i in 1...star{
-    
+
+for i in stride(from: num-1, through:1, by: -1){
+    for i in 1...i{
+        print("*", terminator: "")
+    }
+    print()
 }
