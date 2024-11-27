8d27ccc1073152543070633ef221615f38545d58
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-27 10:08:55 +0800

    Exe1.4

--- Week4/1.4.playground/Contents.swift
+++ Week4/1.4.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+for i in 1...20{
+    if i % 3 == 0 && i % 5 == 0{
+        print("FizzBuzz")
+    }
+    else if i % 3 == 0{
+       print("Fizz")
+    }
+    else if i % 5 == 0{
+        print("Buzz")
+    }
+    else{
+        print(i)
+    }
+}
