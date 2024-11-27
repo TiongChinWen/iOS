555e428bd0050ef05f3e56531b7b7111b7d9b6e8
TCW27 <tiongchinwen0207@e.neivce.edu.my>
2024-11-27 10:22:03 +0800

    Exe1.5

--- Week4/Exe1.5.playground/Contents.swift
+++ Week4/Exe1.5.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let numbers = [3,5,1,4]
+
+for i in 0..<numbers.count{
+    let number = numbers[i]
+    var histogram = ""
+    
+    for _ in 0..<number{
+        histogram += "*"
+    }
+    print(histogram)
+}
